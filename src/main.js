cbc594a60abd60541737faf9307312c266718423
wen19911030 <wen19911030@gmail.com>
2018-08-10 15:06:32 +0800

    feat:登录界面和svg处理

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue';
+import { Form, FormItem, Input, Button, Loading, Message } from 'element-ui';
+import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
 import App from './App';
 import router from './router';
+import iconSvg from './components/iconSvg';
 
-console.log(process.env.NODE_ENV);
+const comArr = [Form, FormItem, Input, Button, iconSvg];
+comArr.forEach(com => {
+  Vue.component(com.name, com);
+});
+// 自动引入 @/src/icons 下面所有的图标了
+// require.context有三个参数：
+// directory：说明需要检索的目录
+// useSubdirectories：是否检索子目录
+// regExp: 匹配文件的正则表达式
+const requireAll = requireContext => requireContext.keys().map(requireContext);
+const req = require.context('./assets/icons', false, /\.svg$/);
+requireAll(req);
+// Vue.component(FromItem.name, Message);
+// Vue.component(Input.name, Message);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
 
 Vue.config.productionTip = false;
 
---
+++ src/pages/login.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="wraper login-wraper">
+    <el-form class="login-form" autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left">
+      <h3 class="title">vue-element-admin</h3>
+      <el-form-item prop="username">
+        <span class="svg-container svg-container_login">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="off" placeholder="username" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <svg-icon icon-class="password"></svg-icon>
+        </span>
+        <el-input name="password" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="off" placeholder="password"></el-input>
+        <span class="show-pwd" @click="showPwd"><svg-icon :icon-class="eyeStatus" /></span>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+          Sign in
+        </el-button>
+      </el-form-item>
+      <div class="tips">
+        <span style="margin-right:20px;">username: {{loginForm.username}}</span>
+        <span> password: {{loginForm.password}}</span>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+    const validateUsername = function(){};
+		return {
+			loginForm: {
+				username: 'admin',
+				password: 'admin'
+			},
+			loginRules: {
+				username: [
+					{ required: true, message: '请输入用户名', trigger: 'blur' },
+					{ min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+				],
+				password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 8, max: 15, message: '长度在 8 到 15 个字符', trigger: 'blur' }
+        ]
+			},
+			loading: false,
+			pwdType: 'password',
+			eyeStatus: 'eye-open'
+		};
+	},
+	methods: {
+		showPwd() {
+			if (this.pwdType === 'text') {
+				this.pwdType = 'password';
+				this.eyeStatus = 'eye-open';
+			} else {
+				this.pwdType = 'text';
+				this.eyeStatus = 'eye-close';
+			}
+		},
+		handleLogin() {
+      this.loading = true;
+      this.$refs.loginForm.validate((valid) => {
+        console.log(valid);
+          if (valid) {
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+    }
+	}
+};
+</script>
+
+<style lang="scss">
+$bg: #2d3a4b;
+$light_gray: #eee;
+/* reset element-ui css */
+.login-wraper {
+	.el-input {
+		display: inline-block;
+		height: 47px;
+		width: 85%;
+		input {
+			background: transparent;
+			border: 0px;
+			-webkit-appearance: none;
+			border-radius: 0px;
+			padding: 12px 5px 12px 0;
+			color: $light_gray;
+			height: 47px;
+			&:-webkit-autofill {
+				box-shadow: 0 0 0px 1000px $bg inset !important;
+				-webkit-text-fill-color: #fff !important;
+			}
+		}
+	}
+	.el-form-item {
+		border: 1px solid rgba(255, 255, 255, 0.1);
+		background: rgba(0, 0, 0, 0.1);
+		border-radius: 5px;
+		color: #454545;
+	}
+}
+</style>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+.login-wraper {
+	position: fixed;
+	height: 100%;
+	width: 100%;
+	background-color: $bg;
+	.login-form {
+		position: absolute;
+		left: 0;
+		right: 0;
+		width: 520px;
+		padding: 35px 35px 15px 35px;
+		margin: 120px auto;
+		box-sizing: border-box;
+	}
+	.tips {
+		font-size: 14px;
+		color: #fff;
+		margin-bottom: 10px;
+		span {
+			&:first-of-type {
+				margin-right: 16px;
+			}
+		}
+	}
+	.svg-container {
+		padding: 6px 5px 6px 15px;
+		color: $dark_gray;
+		vertical-align: middle;
+		width: 30px;
+		display: inline-block;
+		font-size: 20px;
+	}
+	.title {
+		font-size: 26px;
+		font-weight: 400;
+		color: $light_gray;
+		margin: 0px auto 40px auto;
+		text-align: center;
+		font-weight: bold;
+	}
+	.show-pwd {
+		position: absolute;
+		right: 10px;
+		top: 7px;
+		font-size: 16px;
+		color: $dark_gray;
+		cursor: pointer;
+		user-select: none;
+	}
+}
+</style>
